6d3b63a54a3ae5875fccce5c8f3b437194df4c62
TechWise Solutions <techwisesolutionscol@gmail.com>
2024-11-14 12:57:38 -0500

    Actualizar login.php

--- beta/api/login.php
+++ beta/api/login.php
@@ -19,6 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['username'] = $user['username'];
         $_SESSION['name'] = $user['name'];
+        $_SESSION['profile_pic'] = $user['profile_pic'];
         header("Location: dashboard.php"); // Redirigir al panel de usuario
         exit();
     } else {
